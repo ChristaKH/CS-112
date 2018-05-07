6952a9e745bee18150375e82074fdffedafdd946
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2018-05-07 16:17:36 -0700

    Basic setup of JFrame
    Put together the basic format of the JFrame such as size, layout, etc. Decided to switch between panels when a button is pressed in order to perform the appropriate functions.  Also, Added a skeleton for the Action Listener that will be used for the main page/panel and its 6 buttons. Added the first button to the main panel which allows user to view panel. It is in the beginning stages so it doen't do anything so far.

--- ComicDisplay.java
+++ ComicDisplay.java
@@ -1,1 +1,52 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
 
+public class ComicDisplay extends JFrame
+{
+   public static final int WIDTH = 300;
+   public static final int HEIGHT = 200;
+   private JPanel main;
+   
+   //ActionListener for the main page/panel
+   private class MainListener implements ActionListener
+   {
+      public void actionPerformed( ActionEvent e )
+      {
+         
+      }
+   }
+   
+   //main method to activate the GUI
+   public static void main( String [] args )
+   {
+      ComicDisplay display = new ComicDisplay();
+      display.setVisible( true );  
+   }
+   
+   public ComicDisplay()
+   {
+      //JFrame setup
+      super( "Comic Log" );
+      setSize( WIDTH, HEIGHT );
+      setLayout( new FlowLayout() );
+      setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      
+      //main menu buttons
+      main = new JPanel();
+      main.setLayout( new GridLayout( 2, 2 ) );
+      
+      //Button #1: View Comics
+      JButton viewComics = new JButton( "View Comics" );
+      viewComics.addActionListener( new MainListener() );
+      main.add( viewComics );
+      
+      this.add( main );
+   }
+}
